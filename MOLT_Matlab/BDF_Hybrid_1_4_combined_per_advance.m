2677656ca9b1075c8815e4e53ba9c31b8bfc525c
Stephen White <stewhi@microsoft.com>
2023-12-01 13:46:26 -0500

    Messing around with different update techniques, still needs cleaning.

--- MOLT_Matlab/BDF_Hybrid_1_4_combined_per_advance.m
+++ MOLT_Matlab/BDF_Hybrid_1_4_combined_per_advance.m
@@ -13,15 +13,17 @@ function [u,dudx,dudy] = BDF_Hybrid_1_4_combined_per_advance(u, dudx, dudy, src_
     % 
     % dudy1 = BDF1_ddy_advance_per(u, src_data, x, y, t_n, dx, dy, dt, c, beta_BDF);
 
-    dudx = BDF4_ddx_advance_per(u, src_data, x, y, t_n, dx, dy, dt, c, beta_BDF4);
-
-    dudy = BDF4_ddy_advance_per(u, src_data, x, y, t_n, dx, dy, dt, c, beta_BDF4);
+    % dudx = BDF4_ddx_advance_per(u, src_data, x, y, t_n, dx, dy, dt, c, beta_BDF4);
+    % 
+    % dudy = BDF4_ddy_advance_per(u, src_data, x, y, t_n, dx, dy, dt, c, beta_BDF4);
     
     u(:,:,end) = BDF1_advance_per(u, src_data, x, y, t_n, dx, dy, dt, c, beta_BDF);
 
-    % u_next = double(u(1:end-1,1:end-1,end));
+    u_next = u(1:end-1,1:end-1,end);
     % 
-    % [N_y,N_x] = size(u(:,:,end));
+    [N_y,N_x] = size(u(:,:,end));
+    Nx = N_x - 1; % Number of nodes in the nonperiodic section of the grid
+    Ny = N_y - 1; % Number of nodes in the nonperiodic section of the grid
     % 
     % u_next_fft_x = fft(u_next,N_x-1,2);
     % u_next_fft_y = fft(u_next,N_y-1,1);
@@ -35,11 +37,12 @@ function [u,dudx,dudy] = BDF_Hybrid_1_4_combined_per_advance(u, dudx, dudy, src_
     % dudx_fft = copy_periodic_boundaries(dudx_fft);
     % dudy_fft = copy_periodic_boundaries(dudy_fft);
     % 
+    % dudx = dudx_fft;
+    % dudy = dudy_fft;
+
     % dudx_FD4 = zeros(size(dudx));
     % dudy_FD4 = zeros(size(dudy));
     % 
-    % Nx = N_x - 1; % Number of nodes in the nonperiodic section of the grid
-    % Ny = N_y - 1; % Number of nodes in the nonperiodic section of the grid
     % for i = 1:Nx
     %     i_idx_m2 = i-2;
     %     i_idx_m1 = i-1;
@@ -71,48 +74,54 @@ function [u,dudx,dudy] = BDF_Hybrid_1_4_combined_per_advance(u, dudx, dudy, src_
     % dudx_FD4 = copy_periodic_boundaries(dudx_FD4);
     % dudy_FD4 = copy_periodic_boundaries(dudy_FD4);
     % 
-    % dudx_FD6 = zeros(size(dudx));
-    % dudy_FD6 = zeros(size(dudy));
-    % 
-    % for i = 1:Nx
-    %     i_idx_m3 = i-3;
-    %     i_idx_m2 = i-2;
-    %     i_idx_m1 = i-1;
-    %     i_idx    = i+0;
-    %     i_idx_p1 = i+1;
-    %     i_idx_p2 = i+2;
-    %     i_idx_p3 = i+3;
-    %     i_idx_m3 = mod(i_idx_m3 - 1, Nx) + 1;
-    %     i_idx_m2 = mod(i_idx_m2 - 1, Nx) + 1;
-    %     i_idx_m1 = mod(i_idx_m1 - 1, Nx) + 1;
-    %     i_idx    = mod(i_idx    - 1, Nx) + 1;
-    %     i_idx_p1 = mod(i_idx_p1 - 1, Nx) + 1;
-    %     i_idx_p2 = mod(i_idx_p2 - 1, Nx) + 1;
-    %     i_idx_p3 = mod(i_idx_p3 - 1, Nx) + 1;
-    %     for j = 1:Ny
-    %         j_idx_m3 = j-3;
-    %         j_idx_m2 = j-2;
-    %         j_idx_m1 = j-1;
-    %         j_idx    = j+0;
-    %         j_idx_p1 = j+1;
-    %         j_idx_p2 = j+2;
-    %         j_idx_p3 = j+3;
-    %         j_idx_m3 = mod(j_idx_m3 - 1, Nx) + 1;
-    %         j_idx_m2 = mod(j_idx_m2 - 1, Ny) + 1;
-    %         j_idx_m1 = mod(j_idx_m1 - 1, Ny) + 1;
-    %         j_idx    = mod(j_idx    - 1, Ny) + 1;
-    %         j_idx_p1 = mod(j_idx_p1 - 1, Ny) + 1;
-    %         j_idx_p2 = mod(j_idx_p2 - 1, Ny) + 1;
-    %         j_idx_p3 = mod(j_idx_p3 - 1, Nx) + 1;
-    % 
-    %         dudx_FD6(j_idx,i_idx) = (-1/60*u_next(j_idx,i_idx_m3) + 3/20*u_next(j_idx,i_idx_m2) - 3/4*u_next(j_idx,i_idx_m1) + 3/4*u_next(j_idx,i_idx_p1) - 3/20*u_next(j_idx,i_idx_p2) + 1/60*u_next(j_idx,i_idx_p3)) / (dx);
-    %         dudy_FD6(j_idx,i_idx) = (-1/60*u_next(j_idx_m3,i_idx) + 3/20*u_next(j_idx_m2,i_idx) - 3/4*u_next(j_idx_m1,i_idx) + 3/4*u_next(j_idx_p1,i_idx) - 3/20*u_next(j_idx_p2,i_idx) + 1/60*u_next(j_idx_p3,i_idx)) / (dy);
-    %     end
-    % end
-    % 
-    % dudx_FD6 = copy_periodic_boundaries(dudx_FD6);
-    % dudy_FD6 = copy_periodic_boundaries(dudy_FD6);
-    % 
+    % dudx = dudx_FD4;
+    % dudy = dudy_FD4;
+
+    dudx_FD6 = zeros(size(dudx));
+    dudy_FD6 = zeros(size(dudy));
+
+    for i = 1:Nx
+        i_idx_m3 = i-3;
+        i_idx_m2 = i-2;
+        i_idx_m1 = i-1;
+        i_idx    = i+0;
+        i_idx_p1 = i+1;
+        i_idx_p2 = i+2;
+        i_idx_p3 = i+3;
+        i_idx_m3 = mod(i_idx_m3 - 1, Nx) + 1;
+        i_idx_m2 = mod(i_idx_m2 - 1, Nx) + 1;
+        i_idx_m1 = mod(i_idx_m1 - 1, Nx) + 1;
+        i_idx    = mod(i_idx    - 1, Nx) + 1;
+        i_idx_p1 = mod(i_idx_p1 - 1, Nx) + 1;
+        i_idx_p2 = mod(i_idx_p2 - 1, Nx) + 1;
+        i_idx_p3 = mod(i_idx_p3 - 1, Nx) + 1;
+        for j = 1:Ny
+            j_idx_m3 = j-3;
+            j_idx_m2 = j-2;
+            j_idx_m1 = j-1;
+            j_idx    = j+0;
+            j_idx_p1 = j+1;
+            j_idx_p2 = j+2;
+            j_idx_p3 = j+3;
+            j_idx_m3 = mod(j_idx_m3 - 1, Nx) + 1;
+            j_idx_m2 = mod(j_idx_m2 - 1, Ny) + 1;
+            j_idx_m1 = mod(j_idx_m1 - 1, Ny) + 1;
+            j_idx    = mod(j_idx    - 1, Ny) + 1;
+            j_idx_p1 = mod(j_idx_p1 - 1, Ny) + 1;
+            j_idx_p2 = mod(j_idx_p2 - 1, Ny) + 1;
+            j_idx_p3 = mod(j_idx_p3 - 1, Nx) + 1;
+
+            dudx_FD6(j_idx,i_idx) = (-1/60*u_next(j_idx,i_idx_m3) + 3/20*u_next(j_idx,i_idx_m2) - 3/4*u_next(j_idx,i_idx_m1) + 3/4*u_next(j_idx,i_idx_p1) - 3/20*u_next(j_idx,i_idx_p2) + 1/60*u_next(j_idx,i_idx_p3)) / (dx);
+            dudy_FD6(j_idx,i_idx) = (-1/60*u_next(j_idx_m3,i_idx) + 3/20*u_next(j_idx_m2,i_idx) - 3/4*u_next(j_idx_m1,i_idx) + 3/4*u_next(j_idx_p1,i_idx) - 3/20*u_next(j_idx_p2,i_idx) + 1/60*u_next(j_idx_p3,i_idx)) / (dy);
+        end
+    end
+
+    dudx_FD6 = copy_periodic_boundaries(dudx_FD6);
+    dudy_FD6 = copy_periodic_boundaries(dudy_FD6);
+
+    dudx = dudx_FD6;
+    dudy = dudy_FD6;
+
     % dudx_FD8 = zeros(size(dudx));
     % dudy_FD8 = zeros(size(dudy));
     % 
