5729509d50c0c58aa95596dcd7eb7a10958453a6
Stephen White <stewhi@microsoft.com>
2023-12-20 12:03:25 -0500

    Added gauge correction and derivatives from FD6, split out derivative computation to its own method.

--- MOLT_Matlab/BDF1_combined_per_advance_hybrid_FD6.m
+++ MOLT_Matlab/BDF1_combined_per_advance_hybrid_FD6.m
@@ -1,4 +1,4 @@
-function [u,dudx,dudy] = BDF_Hybrid_1_4_combined_per_advance(u, dudx, dudy, src_data, x, y, t_n, dx, dy, dt, c, beta_BDF,kx_deriv_1,ky_deriv_1)
+function [u,dudx,dudy] = BDF1_combined_per_advance_hybrid_FD6(u, dudx, dudy, src_data, x, y, t_n, dx, dy, dt, c, beta_BDF)
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     % Performs the derivative and field advance function for a 2-D scalar field.
     % The function assumes we are working with a scalar field,
@@ -10,56 +10,9 @@ function [u,dudx,dudy] = BDF_Hybrid_1_4_combined_per_advance(u, dudx, dudy, src_
     u(:,:,end) = BDF1_advance_per(u, src_data, x, y, t_n, dx, dy, dt, c, beta_BDF);
 
     u_next = u(1:end-1,1:end-1,end);
-    % 
-    [N_y,N_x] = size(u(:,:,end));
-    Nx = N_x - 1; % Number of nodes in the nonperiodic section of the grid
-    Ny = N_y - 1; % Number of nodes in the nonperiodic section of the grid
 
-    dudx_FD6 = zeros(size(dudx));
-    dudy_FD6 = zeros(size(dudy));
+    [dudx,dudy] = compute_derivative_FD6_periodic(u_next,dx,dy);
 
-    for i = 1:Nx
-        i_idx_m3 = i-3;
-        i_idx_m2 = i-2;
-        i_idx_m1 = i-1;
-        i_idx    = i+0;
-        i_idx_p1 = i+1;
-        i_idx_p2 = i+2;
-        i_idx_p3 = i+3;
-        i_idx_m3 = mod(i_idx_m3 - 1, Nx) + 1;
-        i_idx_m2 = mod(i_idx_m2 - 1, Nx) + 1;
-        i_idx_m1 = mod(i_idx_m1 - 1, Nx) + 1;
-        i_idx    = mod(i_idx    - 1, Nx) + 1;
-        i_idx_p1 = mod(i_idx_p1 - 1, Nx) + 1;
-        i_idx_p2 = mod(i_idx_p2 - 1, Nx) + 1;
-        i_idx_p3 = mod(i_idx_p3 - 1, Nx) + 1;
-        for j = 1:Ny
-            j_idx_m3 = j-3;
-            j_idx_m2 = j-2;
-            j_idx_m1 = j-1;
-            j_idx    = j+0;
-            j_idx_p1 = j+1;
-            j_idx_p2 = j+2;
-            j_idx_p3 = j+3;
-            j_idx_m3 = mod(j_idx_m3 - 1, Nx) + 1;
-            j_idx_m2 = mod(j_idx_m2 - 1, Ny) + 1;
-            j_idx_m1 = mod(j_idx_m1 - 1, Ny) + 1;
-            j_idx    = mod(j_idx    - 1, Ny) + 1;
-            j_idx_p1 = mod(j_idx_p1 - 1, Ny) + 1;
-            j_idx_p2 = mod(j_idx_p2 - 1, Ny) + 1;
-            j_idx_p3 = mod(j_idx_p3 - 1, Nx) + 1;
-
-            dudx_FD6(j_idx,i_idx) = (-1/60*u_next(j_idx,i_idx_m3) + 3/20*u_next(j_idx,i_idx_m2) - 3/4*u_next(j_idx,i_idx_m1) + 3/4*u_next(j_idx,i_idx_p1) - 3/20*u_next(j_idx,i_idx_p2) + 1/60*u_next(j_idx,i_idx_p3)) / (dx);
-            dudy_FD6(j_idx,i_idx) = (-1/60*u_next(j_idx_m3,i_idx) + 3/20*u_next(j_idx_m2,i_idx) - 3/4*u_next(j_idx_m1,i_idx) + 3/4*u_next(j_idx_p1,i_idx) - 3/20*u_next(j_idx_p2,i_idx) + 1/60*u_next(j_idx_p3,i_idx)) / (dy);
-        end
-    end
-
-    dudx_FD6 = copy_periodic_boundaries(dudx_FD6);
-    dudy_FD6 = copy_periodic_boundaries(dudy_FD6);
-
-    dudx = dudx_FD6;
-    dudy = dudy_FD6;
-    
 end
 
 function u = BDF1_advance_per(v, src_data, x, y, t, ...
