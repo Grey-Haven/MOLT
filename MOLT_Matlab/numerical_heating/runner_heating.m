fdc6d360f89b5b52ac4888ef4ea76116884f1701
Grey Haven <stwhite@gmail.com>
2024-01-19 13:50:31 -0500

    Added more data serialization at the end of runs.

--- MOLT_Matlab/numerical_heating/runner_heating.m
+++ MOLT_Matlab/numerical_heating/runner_heating.m
@@ -19,11 +19,11 @@ plot_at = 50;
 % update_method_title = "Vanilla";
 % update_method_folder = "vanilla";
 
-% update_method_title = "FFT Charge Update, BDF-1 Wave Update, FFT Derivative";
-% update_method_folder = "FFT_charge_BDF_wave_update_FFT_derivative";
+update_method_title = "FFT Charge Update, BDF-1 Wave Update, FFT Derivative";
+update_method_folder = "FFT_charge_BDF_wave_update_FFT_derivative";
 
-update_method_title = "FD6 Charge Update, BDF-1 Wave Update, FD6 Derivative";
-update_method_folder = "FD6_charge_BDF_wave_update_FD6_derivative";
+% update_method_title = "FD6 Charge Update, BDF-1 Wave Update, FD6 Derivative";
+% update_method_folder = "FD6_charge_BDF_wave_update_FD6_derivative";
 
 modification = "no_mod";
 % modification = "FFT_splitting_err";
@@ -56,6 +56,21 @@ var_hist_64 = v_elec_var_hist;
 temp_hist_64 = temp_hist;
 gauge_error_array_64 = gauge_error_array(:,2);
 
+g = 128;
+variable_setup_heating;
+asym_euler_particle_heating_solver_heating;
+t_128 = ts;
+var_hist_128 = v_elec_var_hist;
+temp_hist_128 = temp_hist;
+gauge_error_array_128 = gauge_error_array(:,2);
+
+g = 256;
+variable_setup_heating;
+asym_euler_particle_heating_solver_heating;
+t_256 = ts;
+var_hist_256 = v_elec_var_hist;
+temp_hist_256 = temp_hist;
+gauge_error_array_256 = gauge_error_array(:,2);
 figure;
 
 x0=200;
@@ -66,25 +81,29 @@ set(gcf,'position',[x0,y0,width,height]);
 
 subplot(1,2,1);
 plot(t_16,temp_hist_16);
-% hold on;
-% plot(t_32,temp_hist_32);
-% plot(t_64,temp_hist_64);
-% hold off;
+hold on;
+plot(t_32,temp_hist_32);
+plot(t_64,temp_hist_64);
+plot(t_128,temp_hist_128);
+plot(t_256,temp_hist_256);
+hold off;
 xlabel("t",'FontSize',28);
 ylabel("Electron Temperature [K]",'Interpreter','latex','FontSize',28);
-% legend("16x16", "32x32", "64x64",'FontSize',18);
+legend("16x16", "32x32", "64x64", "128x128", "256x256", 'FontSize',18);
 title("Electron Temperature",'FontSize',32);
 
 subplot(1,2,2);
 plot(t_16,gauge_error_array_16);
-% hold on;
-% plot(t_32,gauge_error_array_32);
-% plot(t_64,gauge_error_array_64);
-% hold off;
+hold on;
+plot(t_32,gauge_error_array_32);
+plot(t_64,gauge_error_array_64);
+plot(t_128,gauge_error_array_128);
+plot(t_256,gauge_error_array_256);
+hold off;
 xlabel("t",'FontSize',28);
 ylabel("$||\frac{\partial \phi}{\partial t} + \nabla \cdot \textbf{A}||_2$",'Interpreter','latex','FontSize',28);
-% legend("16x16", "32x32", "64x64",'FontSize',18);
+legend("16x16", "32x32", "64x64", "128x128", "256x256", 'FontSize',18);
 title("Gauge Error",'FontSize',32);
 
-sgtitle("16x16 Vanilla / FD6 Gauge Correction",'FontSize', 48)
+sgtitle("FFT Continuity",'FontSize', 48);
 
