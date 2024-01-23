b4c319c007f8e3abd784f7181b8d9526b2b05755
GreyHaven <stwhite92@gmail.com>
2024-01-22 22:40:21 -0500

    Fixed a bug in the FFT derivative methods.

--- MOLT_Matlab/utility_functions/compute_ddx_FFT.m
+++ MOLT_Matlab/utility_functions/compute_ddx_FFT.m
@@ -4,8 +4,10 @@ function dudx_fft = compute_ddx_FFT(u,kx_deriv_1)
     %%%%%%%%%%%%%%%%%%%%%%%%%
 
     [N_y,N_x] = size(u(:,:,end));
+    
+    u_domain = u(1:end-1,1:end-1,end);
 
-    u_fft_x = fft(u_next,N_x-1,2);
+    u_fft_x = fft(u_domain,N_x-1,2);
 
     dudx_fft = zeros(N_y,N_x);
 
--- MOLT_Matlab/utility_functions/compute_ddy_FFT.m
+++ MOLT_Matlab/utility_functions/compute_ddy_FFT.m
@@ -4,8 +4,10 @@ function dudy_fft = compute_ddy_FFT(u,ky_deriv_1)
     %%%%%%%%%%%%%%%%%%%%%%%%%
 
     [N_y,N_x] = size(u(:,:,end));
+    
+    u_domain = u(1:end-1,1:end-1,end);
 
-    u_fft_y = fft(u_next,N_y-1,1);
+    u_fft_y = fft(u_domain,N_y-1,1);
 
     dudy_fft = zeros(N_y,N_x);
 
