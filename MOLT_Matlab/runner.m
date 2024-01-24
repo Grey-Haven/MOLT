c9bb0a30db6756aaaf397c43a1712dce8f84b597
GreyHaven <stwhite92@gmail.com>
2024-01-24 07:39:47 -0500

    Cleaned up the blob runner.

--- MOLT_Matlab/runner.m
+++ MOLT_Matlab/runner.m
@@ -4,53 +4,28 @@ addpath(genpath([fileparts(pwd)]));
 addpath(genpath([fileparts(pwd), '/utility_functions']));
 addpath(genpath([fileparts(pwd), '/rho_updaters']));
 
-grid_refinement = [16,32,64]; % Run FFT BDF BDF for 64x64
+grid_refinement = [64]; % Run FFT BDF BDF for 64x64
 CFLs = [1];
 particle_count_multipliers = [10];
 
 debug = true;
 
-enable_plots = true;
-write_csvs = true;
+enable_plots = false;
+write_csvs = false;
 plot_at = 500;
 
 
-update_method_title = "Vanilla";
-update_method_folder = "vanilla";
+% update_method_title = "Vanilla";
+% update_method_folder = "vanilla";
 
-% update_method_title = "FFT Charge Update, BDF-1 Wave Update, FD6 Derivative";
-% update_method_folder = "FFT_charge_BDF_wave_update_FD6_derivative";
-
-% update_method_title = "FFT Charge Update, BDF-1 Wave Update, BDF-1 Derivative";
-% update_method_folder = "FFT_charge_BDF_wave_update_BDF_derivative";
-
-% update_method_title = "FFT Charge Update, BDF-1 Wave Update, FFT Derivative";
-% update_method_folder = "FFT_charge_BDF_wave_update_FFT_derivative";
-
-% update_method_title = "FD6 Charge Update, FFT Wave Update, FFT Derivative";
-% update_method_folder = "FD6_charge_FFT_wave_update_FFT_derivative";
-
-% update_method_title = "FD6 Charge Update, BDF-1 Wave Update, FFT Derivative";
-% update_method_folder = "FD6_charge_BDF_wave_update_FFT_derivative";
-
-% update_method_title = "FD6 Charge Update, BDF-1 Wave Update, FD6 Derivative, Gauge Corrected";
+% update_method_title = "FD6 Charge Update, BDF-1 Wave Update, FD6 Derivative";
 % update_method_folder = "FD6_charge_BDF_wave_update_FD6_derivative";
 
-% update_method_title = "FD6 Charge Update, BDF-1 Wave Update, FFT Derivative";
-% update_method_folder = "FD6_charge_BDF_wave_update_FFT_derivative";
-
-% update_method_title = "Vanilla Particle Update, FFT Wave Update";
-% update_method_folder = "vanilla_particle_fft_wave_update";
-
-% update_method_title = "Non-Iterative FFT";
-% update_method_folder = "non_iterative_fft";
-
-% update_method_title = "Iterative FFT";
-% update_method_folder = "iterative_fft";
+update_method_title = "FFT Charge Update, BDF-1 Wave Update, FFT Derivative";
+update_method_folder = "FFT_charge_BDF_wave_update_FFT_derivative";
     
-% modification = "no_mod";
-% modification = "FFT_splitting_err";
-modification = "correct_gauge";
+modification = "no_mod";
+% modification = "correct_gauge_fft";
 
 mesh_independent_variable_setup;
 
