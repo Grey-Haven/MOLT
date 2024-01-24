8990f58a51f6ccdf0bd8eec852633b1b0affa4a1
Stephen White <stewhi@microsoft.com>
2024-01-24 17:38:25 -0500

    Added a commented out section for plotting different ways of measuring Gauss' Law.

--- MOLT_Matlab/utility_functions/create_plots.m
+++ MOLT_Matlab/utility_functions/create_plots.m
@@ -52,26 +52,51 @@ xlim([x(1),x(end)]);
 ylim([y(1),y(end)]);
 axis square;
 
-
-% subplot(2,3,5);
-% surf(x,y,psi_A);
-% xlabel("x");
-% ylabel("y");
-% title("$\phi_A$",'Interpreter','latex');
-% xlim([x(1),x(end)]);
-% ylim([y(1),y(end)]);
-% 
-% subplot(2,3,6);
-% surf(x,y,psi_C);
-% xlabel("x");
-% ylabel("y");
-% title("$\phi_C$",'Interpreter','latex');
-% xlim([x(1),x(end)]);
-% ylim([y(1),y(end)]);
-
 sgtitle({update_method_title + " method", "Grid: " + tag + ", CFL: " + CFL + ", Particle Multiplier: " + particle_count_multiplier, "t = " + num2str(t_n,'%.4f')});
 
 drawnow;
 
 currFrame = getframe(gcf);
 writeVideo(vidObj, currFrame);
+
+% Plotting diagnostics for Gauss' Law
+
+% subplot(1,3,1);
+% surf(x,y,ddx_E1 + ddy_E2);
+% title("$\nabla \cdot \textbf{E}$",'FontSize',24);
+% subplot(1,3,2);
+% surf(x,y,rho_mesh/sigma_1);
+% title("$\frac{\rho}{\sigma_1}$",'FontSize',24);
+% subplot(1,3,3);
+% surf(x,y,ddx_E1 + ddy_E2 - rho_mesh/sigma_1);
+% title("$\nabla \cdot \textbf{E} - \frac{\rho}{\sigma_1}$",'FontSize',24);
+% 
+% subplot(3,3,1);
+% surf(x,y,ddx_E1 + ddy_E2);
+% title("$\nabla \cdot \textbf{E}$",'FontSize',24);
+% subplot(3,3,2);
+% surf(x,y,rho_mesh/sigma_1);
+% title("$\frac{\rho}{\sigma_1}$",'FontSize',24);
+% subplot(3,3,3);
+% surf(x,y,ddx_E1 + ddy_E2 - rho_mesh/sigma_1);
+% title("$\nabla \cdot \textbf{E} - \frac{\rho}{\sigma_1}$",'FontSize',24);
+% 
+% subplot(3,3,4);
+% surf(x,y,-ddt_div_A - laplacian_phi_FFT);
+% title("$-\frac{\nabla \cdot \textbf{A}^{n+1} - \nabla \cdot \textbf{A}^{n}}{\Delta t} - \Delta \phi^{n+1}$",'FontSize',24);
+% subplot(3,3,5);
+% surf(x,y,rho_mesh / sigma_1);
+% title("$\frac{\rho^{n+1}}{\sigma_1}$",'interpreter','latex','FontSize',24);
+% subplot(3,3,6);
+% surf(x,y,-ddt_div_A - laplacian_phi_FFT - rho_mesh / sigma_1);
+% title("$-\frac{\nabla \cdot \textbf{A}^{n+1} - \nabla \cdot \textbf{A}^{n}}{\Delta t} - \Delta \phi^{n+1} - \frac{\rho^{n+1}}{\sigma_1}$",'FontSize',24);
+% 
+% subplot(3,3,7);
+% surf(x,y,1/kappa^2 * ddt2_phi - laplacian_phi_FFT);
+% title("$\frac{1}{\kappa^2}\frac{\phi^{n+1} - 2\phi^{n} + \phi^{n-1}}{\Delta t^2} - \Delta \phi^{n+1}$",'FontSize',24);
+% subplot(3,3,8);
+% surf(x,y,rho_mesh / sigma_1);
+% title("$\frac{\rho^{n+1}}{\sigma_1}$",'interpreter','latex','FontSize',24);
+% subplot(3,3,9);
+% surf(x,y,1/kappa^2 * ddt2_phi - laplacian_phi_FFT - rho_mesh / sigma_1);
+% title("$\frac{1}{\kappa^2}\frac{\phi^{n+1} - 2\phi^{n} + \phi^{n-1}}{\Delta^2} - \Delta \phi^{n+1} - \frac{\rho^{n+1}}{\sigma_1}$",'FontSize',24);
