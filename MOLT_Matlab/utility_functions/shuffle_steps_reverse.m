28a931423c7ebcb06862a4130883d17156428747
Stephen White <stewhi@microsoft.com>
2024-02-06 19:15:11 -0500

    Added a reverse shuffler

---
+++ MOLT_Matlab/utility_functions/shuffle_steps_reverse.m
@@ -0,0 +1,20 @@
+function u = shuffle_steps(u)
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Performs the data swap required prior to advancing to the next time step.
+    %
+    % This performs the required data transfers for the multistep methods, which store
+    % a total of several levels in time. The function assumes we are working with a scalar field,
+    % but it can be called on the scalar components of a vector field.
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    
+    % Identify the number of time levels
+    num_dimensions = ndims(u);
+    num_levels = size(u,num_dimensions);
+
+    otherdims = repmat({':'},1,num_dimensions-1);
+    
+    % Transfer the time history starting from the oldest available data
+    for level = num_levels:-1:2
+        u(otherdims{:},level) = u(otherdims{:},level - 1);
+    end
+end
