c26e00c57fbd885f27ceabf09de6b0d4dd24d144
Stephen White <stewhi@microsoft.com>
2024-05-08 10:51:13 -0400

    Tweaked the DIRK2 wave solver

--- MOLT_Matlab/second_order/wave_solvers/update_waves_DIRK2.m
+++ MOLT_Matlab/second_order/wave_solvers/update_waves_DIRK2.m
@@ -15,9 +15,13 @@ A1_src_hist(:,:,end)   = A1_src;
 A2_src_hist(:,:,end-1) = A2_src_prev;
 A2_src_hist(:,:,end)   = A2_src;
 
-ddt_psi = (psi(:,:,end-1) - psi(:,:,end-2)) / dt;
-ddt_A1  = (A1(:,:,end-1)  - A1(:,:,end-2)) / dt;
-ddt_A2  = (A2(:,:,end-1)  - A2(:,:,end-2)) / dt;
+% ddt_psi = (psi(:,:,end-1) - psi(:,:,end-2)) / dt;
+% ddt_A1  = (A1(:,:,end-1)  - A1(:,:,end-2)) / dt;
+% ddt_A2  = (A2(:,:,end-1)  - A2(:,:,end-2)) / dt;
+
+ddt_psi = ddt_psi_hist(:,:,end);
+ddt_A1  = ddt_A1_hist(:,:,end);
+ddt_A2  = ddt_A2_hist(:,:,end);
 
 [psi_next, ddt_psi_next] = DIRK2_advance_per(psi(:,:,end-1), ddt_psi, psi_src_hist, kappa, dt, kx_deriv_2, ky_deriv_2);
 [A1_next,  ddt_A1_next ] = DIRK2_advance_per(A1(:,:,end-1) , ddt_A1 , A1_src_hist , kappa, dt, kx_deriv_2, ky_deriv_2);
@@ -26,6 +30,10 @@ ddt_A2  = (A2(:,:,end-1)  - A2(:,:,end-2)) / dt;
 psi(:,:,end) = psi_next;
 A1(:,:,end)  = A1_next;
 A2(:,:,end)  = A2_next;
+
+ddt_psi_hist(:,:,end) = ddt_psi_next;
+ddt_A1_hist(:,:,end)  = ddt_A1_next;
+ddt_A2_hist(:,:,end)  = ddt_A2_next;
 
 %---------------------------------------------------------------------
 % 5.2.1. Compute their derivatives using the FFT
