3263f0b38a2316fc277f2cbfe1b8c3d08638be59
Grey Haven <stwhite@gmail.com>
2024-07-29 16:58:49 -0400

    Updated energy function to return kinetic, potential, and total energy.

--- MOLT_Matlab/utility_functions/compute_total_energy.m
+++ MOLT_Matlab/utility_functions/compute_total_energy.m
@@ -1,4 +1,4 @@
-function total_energy = compute_total_energy(phi, A1, A2, x1, x2, P1, P2, x, y, q_s, m_s)    
+function [kinetic_energy, potential_energy, total_energy] = compute_total_energy(phi, A1, A2, x1, x2, P1, P2, x, y, q_s, m_s)    
     % Extract the grid spacings (assumed to be uniform)
     dx = x(2) - x(1);
     dy = y(2) - y(1);
@@ -9,10 +9,12 @@ function total_energy = compute_total_energy(phi, A1, A2, x1, x2, P1, P2, x, y, 
     A2_p = gather_2D_vectorized(A2, x1, x2, x, y, dx, dy);
     
     % Now we can build the terms that give the energy
-    tmp = (P1 - q_s*A1_p).^2;
-    tmp = tmp + (P2 - q_s*A2_p).^2;
-    tmp = tmp * 1./(2*m_s);
-    tmp = tmp + q_s*phi_p;
-
-    total_energy = sum(tmp);
+    T = (P1 - q_s*A1_p).^2;
+    T = T + (P2 - q_s*A2_p).^2;
+    T = T * 1./(2*m_s);
+    V = q_s*phi_p;
+    
+    kinetic_energy = sum(T);
+    potential_energy = sum(V);
+    total_energy = kinetic_energy + potential_energy;
 end
