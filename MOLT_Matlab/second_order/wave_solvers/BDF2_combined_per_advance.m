237653b7dcd2fa2eac6b2c5aa34ba09297dbcd44
Grey Haven <stwhite92@gmail.com>
2024-10-02 19:27:19 -0400

    Added the BDF1, BDF2 update waves hybrid for FD6 and FFT, as well as update waves for pure MOLT BDF2 and CDF2

--- MOLT_Matlab/second_order/wave_solvers/BDF2_combined_per_advance.m
+++ MOLT_Matlab/second_order/wave_solvers/BDF2_combined_per_advance.m
@@ -11,7 +11,7 @@ function [u,dudx,dudy] = BDF2_combined_per_advance(u, dudx, dudy, src_data, x, y
 
     dudy(:,:,end) = BDF2_ddy_advance_per(u, src_data, x, y, t_n, dx, dy, dt, c, beta_BDF);
     
-    u(:,:,end) = BDF2_advance_per(u, src_data, x, y, t_n, dx, dy, dt, c, beta_BDF);
+    u(:,:,end) = BDF2_per_advance(u, src_data, x, y, dx, dy, dt, c, beta_BDF);
     
 end
 
@@ -63,30 +63,3 @@ function ddy = BDF2_ddy_advance_per(v, src_data, x, y, t, ...
     % Shuffle is performed outside this function
     
 end
-
-function u = BDF2_advance_per(v, src_data, x, y, t, ...
-                              dx, dy, dt, c, beta)
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    % Calculates a solution to the wave equation using the first-order BDF method. 
-    % This function accepts the mesh data v and src_data.
-    %
-    % Source function is implicit in the BDF method.
-    %
-    % Note that all arrays are passed by reference, unless otherwise stated.
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-    % Build the convolution integral over the time history R
-    alpha = beta/(c*dt);
-
-    R = 8/3*v(:,:,end-1) - 22/9*v(:,:,end-2) + 8/9*v(:,:,end-3) - 1/9*v(:,:,end-4) + 1/(alpha^2)*src_data(:,:);
-
-    % Invert the y operator and apply to R, storing in tmp
-    tmp = get_L_y_inverse_per(R, x, y, dx, dy, dt, c, beta);
-
-    % Invert the x operator and apply to tmp, then store in the new time level
-    u = get_L_x_inverse_per(tmp, x, y, dx, dy, dt, c, beta);
-
-    % Shuffle is performed outside this function
-    
-end
-    
---
+++ MOLT_Matlab/wave_solvers/BDF1_update_waves_hybrid_FD6.m
@@ -0,0 +1,35 @@
+psi_src = 1/sigma_1 * rho_mesh(:,:,end);
+A1_src  =   sigma_2 *  J1_mesh(:,:,end);
+A2_src  =   sigma_2 *  J2_mesh(:,:,end);
+
+%---------------------------------------------------------------------
+% 5.2.1. Advance the waves
+%---------------------------------------------------------------------
+
+psi(:,:,end) = BDF1_per_advance(psi, psi_src, x, y, dx, dy, dt, kappa, beta_BDF2);
+
+A1(:,:,end)  = BDF1_per_advance(A1 , A1_src , x, y, dx, dy, dt, kappa, beta_BDF2);
+
+A2(:,:,end)  = BDF1_per_advance(A2 , A2_src , x, y, dx, dy, dt, kappa, beta_BDF2);
+
+%---------------------------------------------------------------------
+% 5.2.1. Compute their derivatives using the FD6
+%---------------------------------------------------------------------
+
+ddx_psi(1:end-1,1:end-1,end) = compute_ddx_FD6_per(psi(1:end-1,1:end-1,end), dx);
+ddy_psi(1:end-1,1:end-1,end) = compute_ddy_FD6_per(psi(1:end-1,1:end-1,end), dy);
+
+ddx_A1(1:end-1,1:end-1,end)  = compute_ddx_FD6_per(A1(1:end-1,1:end-1,end) , dx);
+ddy_A1(1:end-1,1:end-1,end)  = compute_ddy_FD6_per(A1(1:end-1,1:end-1,end) , dy);
+
+ddx_A2(1:end-1,1:end-1,end)  = compute_ddx_FD6_per(A2(1:end-1,1:end-1,end) , dx);
+ddy_A2(1:end-1,1:end-1,end)  = compute_ddy_FD6_per(A2(1:end-1,1:end-1,end) , dy);
+
+ddx_psi(:,:,end) = copy_periodic_boundaries(ddx_psi(:,:,end));
+ddy_psi(:,:,end) = copy_periodic_boundaries(ddy_psi(:,:,end));
+
+ddx_A1(:,:,end) = copy_periodic_boundaries(ddx_A1(:,:,end));
+ddy_A1(:,:,end) = copy_periodic_boundaries(ddy_A1(:,:,end));
+
+ddx_A2(:,:,end) = copy_periodic_boundaries(ddx_A2(:,:,end));
+ddy_A2(:,:,end) = copy_periodic_boundaries(ddy_A2(:,:,end));
